0730127da33a7f0e47da997e37a44fac10b42ae0
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-03-21 16:00:42 -0300

    CURSO DE PYTHON

---
+++ ex003.py
@@ -0,0 +1,4 @@
+var = input("digite algo :")
+print('é um numero ?', var.isnumeric())
+print('é composto de letra maiúscula ?', var.isupper())
+print('é composto de letras minúsculas ?',  var.islower())

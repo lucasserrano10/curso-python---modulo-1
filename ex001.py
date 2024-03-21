cba8ef1e9e8164e680c536dd5f8b6bec349c5182
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-03-21 14:19:33 -0300

    CURSO PYTHON - MÓDULO 1

--- ex001.py
+++ ex001.py
@@ -2,4 +2,6 @@ nome = input("NOME :")
 idade= int(input("IDADE:"))
 peso = float(input("PESO:"))
 
-print(f'Olá {nome}, de idade :{idade} e peso : {peso}kg, Seja Bem Vindo(a) !')
+print(f'Olá {nome}, de idade :{idade} anos e peso : {peso}kg, Seja Bem Vindo(a) !')
+
+print(type(nome))
---
+++ ex002.py
@@ -0,0 +1,4 @@
+n1 = float(input("número 1 :"))
+n2 = float(input("número 2 :"))
+
+print(f'a soma entre os numeros {n1} e {n2} é {n1+n2}')

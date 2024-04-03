92b3c91ac9aeba383fe94d2d44ab4aa45534fcd5
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-04-03 11:57:13 -0300

    while exercicios

--- ex060.py
+++ ex060.py
@@ -1,15 +1,27 @@
+qtdadeNumeros = 0
+somaValores = 0
+maior = 0
+menor = 10000000000000000000000
+resp = 'S'
+while resp == 'S':
+    num = int(input('DIGITE UM NÚMERO :'))
+    qtdadeNumeros += 1
+    somaValores += num
+    if qtdadeNumeros == 1:
+        maior == menor == num
+    if num >= maior:
+        maior = num
+    if num <= menor:
+        menor = num
+    resp = str(input('QUER CONTINUAR [S/N] : ')).upper().strip()
+media = (somaValores/qtdadeNumeros)
+print(f'-'*20)
+print(f'VOCE DIGITOU {qtdadeNumeros} NÚMEROS')
+print(f'-'*20)
+print(f'A MÉDIA DOS VALORES É {media}')
+print(f'-'*20)
+print(f'MAIOR NÚMERO DIGITADO FOI {maior}')
+print(f'-'*20)
+print(f'MENOR NÚMERO DIGITADO FOI {menor}')
 
-contadorNumeros = 0
-somaNumeros = 0
-aux = 0
-while aux == 1:
-    numero = int(input('DIGITE UM NÚMERO :'))
-    somaNumeros += numero
-    contadorNumeros += 1
-    escolha = int(input('QUER ESCREVER MAIS NÚMEROS [1-sim/2-não] :'))
-    if escolha == 2:
-        0 == aux
-    else:
-        1 == aux
-
-print(f'A MÉDIA DOS NÚMEROS FORAM')
+print('ACABOU')
